d17f58afec8f62fd3e8a7ee6260b6d74b90565c0
nimrab <nimrab.0@gmail.com>
2022-01-01 18:54:59 +0300

    start screen bones

---
+++ src/components/NextButton/NextButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import css from './Button.module.scss'
+
+export const Button = () => {
+    return (
+        <div className={css.button}>
+
+        </div>
+    )
+}
+
